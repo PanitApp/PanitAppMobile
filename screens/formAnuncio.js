6fd9c25740443b87a65cb5e957b784b0164b5b0f
Farid <difcortesgu@unal.edu.co>
2020-11-22 15:37:32 -0500

    :sparkles: listo subir archivos en anuncios

--- screens/formAnuncio.js
+++ screens/formAnuncio.js
@@ -1,32 +1,32 @@
 import React, { useState } from 'react';
-import { StyleSheet, TextInput, View, Text, Dimensions, TouchableOpacity } from 'react-native';
-import { Form, Item } from 'native-base'
+import { StyleSheet, TextInput, View, Text, Dimensions, TouchableOpacity, Alert } from 'react-native';
+import { Form, Spinner } from 'native-base'
 import { Formik } from 'formik';
 import * as yup from 'yup'
 import firebase from '../database/firebase';
 import * as DocumentPicker from 'expo-document-picker';
-
 const { width: WIDTH } = Dimensions.get('window')
 
 
 export default function formAnuncio({ crearAnuncio, curso }) {
 
-  //const [fileUrl, setFileUrl] = useState(null);
-  const [singleFile, setSingleFile] = useState(null);
+  const [singleFile, setSingleFile] = useState("");
+  const [FileName, setFileName] = useState("");
+  const [loadingFile, setLoadingFile] = useState(false);
+  const [fileProgress, SetFileProgress] = useState(0.0);
 
   const selectFile = async () => {
+    setLoadingFile(true)
     // Opening Document Picker to select one file
     const res = await DocumentPicker.getDocumentAsync({})
-    console.log('res : ' + JSON.stringify(res));
-    
-    // Why are we using XMLHttpRequest? See:
-    // https://github.com/expo/expo/issues/2402#issuecomment-443726662
+    // This is to get the blob of the file  
     const blob = await new Promise((resolve, reject) => {
       const xhr = new XMLHttpRequest();
       xhr.onload = function() {
         resolve(xhr.response);
       };
       xhr.onerror = function(e) {
+        setLoadingFile(false)
         console.log(e);
         reject(new TypeError('Network request failed'));
       };
@@ -34,18 +34,42 @@ export default function formAnuncio({ crearAnuncio, curso }) {
       xhr.open('GET', res.uri, true);
       xhr.send(null);
     });
+
+    // this is to send the file to firebase
     const ref = firebase
       .storage()
       .ref()
       .child(res.name);
-    const snapshot = await ref.put(blob);
+    const uploadTask = ref.put(blob)
 
-    // We're done with the blob, close and release it
-    blob.close();
-
-    let f = await snapshot.ref.getDownloadURL();
-    console.log(f)
-    setSingleFile(f)
+    uploadTask.on('state_changed', function(snapshot){
+      // Observe state change events such as progress, pause, and resume
+      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      SetFileProgress(progress)
+      switch (snapshot.state) {
+        case firebase.storage.TaskState.PAUSED: // or 'paused'
+          console.log('Upload is paused');
+          break;
+        case firebase.storage.TaskState.RUNNING: // or 'running'
+          console.log('Upload is running');
+          break;
+      }
+    }, function(error) {
+      setFileName("")
+      setLoadingFile(false)
+      SetFileProgress(0)
+      console.log(error)
+    }, function() {
+      // Handle successful uploads on complete
+      blob.close();
+      uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+        setLoadingFile(false)
+        setSingleFile(downloadURL)
+        setFileName(res.name)
+        Alert.alert("Archivo subido correctamente!")
+      });
+    });
   };
 
   const validationSchema = yup.object({
@@ -54,7 +78,6 @@ export default function formAnuncio({ crearAnuncio, curso }) {
 
   return (
     <View style={styles.container}>
-      <Text>{singleFile}</Text>
       <Formik
         style={styles.form}
         validationSchema={validationSchema}
@@ -66,7 +89,7 @@ export default function formAnuncio({ crearAnuncio, curso }) {
           let anuncio = {
             descripcion: values.descripcion,
             fecha_publicacion: new Date().toISOString(),
-            archivo: "",
+            archivo: singleFile,
             id_curso: parseInt(curso.id)
           };
           crearAnuncio({ variables: anuncio })
@@ -83,17 +106,28 @@ export default function formAnuncio({ crearAnuncio, curso }) {
               onBlur={props.handleBlur('descripcion')}
               value={props.values.descripcion}
             />
+            {loadingFile?(
+              <View> 
+                <Spinner />
+                <Text>Subiendo archivo: {fileProgress}</Text>
+              </View> 
+            ):(
+              <View>
+                <Text>{FileName}</Text>
+                <TouchableOpacity style={styles.btnLogin}>
+                  <Text style={styles.text} onPress={selectFile}>
+                    Añadir archivo
+                  </Text>
+                  {/* <Icon name="upload" style={{ color: '#037E85' }} /> */}
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.btnLogin} disabled={loadingFile}>
+                  <Text style={styles.text} onPress={props.handleSubmit}>
+                    Crear anuncio
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            )}
 
-            <TouchableOpacity style={styles.btnLogin}>
-              <Text style={styles.text} onPress={selectFile}>
-                Añadir archivos
-              </Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.btnLogin}>
-              <Text style={styles.text} onPress={props.handleSubmit}>
-                Crear anuncio
-              </Text>
-            </TouchableOpacity>
           </Form>
         )}
       </Formik>
